15fc9763ad9b004b9b7bc5765404a4a12bb4acde
HAckeRSHIL <hackwithharshil@gmail.com>
2020-07-10 18:26:01 +0530

    Init command

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+console.log("Client side javascript file is loaded!");
+
+const search = document.querySelector("#search");
+const btn = document.querySelector("#btn");
+const m1 = document.querySelector("#m1");
+const m2 = document.querySelector("#m2");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  const path = "http://localhost:3000/weather?address=" + location;
+  m1.textContent = "Loading.....";
+  fetch(path).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        m1.textContent = "Error : " + data.error;
+      } else {
+        m1.textContent = "Forecast : " + data.forecast;
+        m2.textContent = "Location : " + data.location;
+      }
+    });
+  });
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require("request");
+const baseurl = "https://api.weatherbit.io/v2.0/current?city=";
+const suffixurl = "&key=2c2f91795d6841b1bddffc18ce9908a8";
+forecast = (city, callback) => {
+  const url = baseurl + city + suffixurl;
+  request({ url: url, json: true }, (error, resp, body) => {
+    if (error) {
+      callback(1);
+    } else if (resp.body) {
+      callback(resp.body.data[0].weather.description, city);
+    } else {
+      callback(2);
+    }
+  });
+};
+module.exports = forecast;
